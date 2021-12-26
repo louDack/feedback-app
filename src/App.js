277d1288e9f047c1d6001649043bf024cbc93caa
Deric Siu <dddsiu@derics-mbp.lan>
2021-12-25 22:15:42 -0800

    Wrote how js map() function works 3 diff ways.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import Header from "./components/Header"
 
 function App() {
+  const background = {backgroundColor: 'blue'}
+  const feedbackList = [
+    {name: 'Anri Okita', id: 1},
+    {name: 'Hitomi Tanaka', id: 2},
+    {name: 'Nanako Mori', id: 3}
+  ]
+  // pass style into prop.
+
   return (
-    <>
+    <div style={background}>
       <Header text='Hello World'/>
-      <div className="App">
-        My App
-      </div>    
-    </>
+      <h1>Hello, you fackin' pussy!</h1>
+
+      {
+        feedbackList.map((feedback, index) => (
+          <div>{feedback.name}</div>
+        ))
+      }
+      
+      <br></br>
+      
+      {
+        [
+          <div>{feedbackList[0].name}</div>,
+          <div>{feedbackList[1].name}</div>,
+          <div>{feedbackList[2].name}</div>
+        ]
+      }
+
+      <br></br>
+      
+      <div>{feedbackList[0].name}</div>
+      <div>{feedbackList[1].name}</div>
+      <div>{feedbackList[2].name}</div>
+
+
+    </div>
   );
 }
 
