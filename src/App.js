102fc22a1fc5f7314de522507ab90fbb82af4254
Deric Siu <dddsiu@derics-mbp.lan>
2021-12-27 01:28:19 -0800

    Added card styling and feedbackItem component. Had to debug a misunderstood margin definition. rip.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
 import Header from "./components/Header"
+import FeedbackItem from "./components/FeedbackItem"
 
 function App() {
   const test = true
-  const background = {backgroundColor: 'blue'}
+  const background = {
+    backgroundColor: 'purple', 
+    height: "100vh"
+  }
+
   const feedbackList = [
     {name: 'Anri Okita', id: 1},
     {name: 'Hitomi Tanaka', id: 2},
     {name: 'Nanako Mori', id: 3}
   ]
 
-  let color, textColor = {color: 'green'}
-  let moon = <div style={{backgroundColor: 'silver'}}>HAIL MOON</div>
-
   // Goal: Conditional everything!
   
   return (
-    <div style={background}>
-      <Header text='World'/>
-      <h1>Fuck you, fackin' pussy!</h1>
-      
-      {/* What the fk is this? */}
-      {/* Conditional Rendering via ternary operator */}
-      {color = test ? 'red' : 'blue'}
-      {console.log(color)}
+    <div className='container' style={background}>
 
-      {/* Dynamic styling */}
-      <div style={textColor}>I'm dynamic styling!</div>
-      <Header text={'TIMOTHY'} textColor={'purple'} />
-      
-      {/* Error warning if passing in wrong type into props. */}
-      {/* <Header text={123} textColor={'purple'} /> */}
-
-      {test ? 'suck' : 'Lick booogers.'}
-      {!test ? <div style={{color: 'red'}}><h1>HERE I AM</h1></div> : <h5>Go forth eorlingas!</h5>}
-
-      {test && <div style={{backgroundColor: 'goldenrod'}}>HAIL SUN</div>}
-      {test && moon}
-
+      <FeedbackItem />
+      <FeedbackItem />
 
       {feedbackList.map((feedback, index) => (
           <Header key={index} text={feedback.name} />
---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../index.css'
+
+const FeedbackItem = () => {
+    return (
+        <div className="card">
+            <div className="rating">10</div>
+            <div className="text-display">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, maiores.</div>
+        </div>
+    )
+}
+
+export default FeedbackItem
