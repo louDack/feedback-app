2abc182ba8ebef96f1f8d6afb73b0ba260490287
Deric Siu <dddsiu@derics-mbp.lan>
2022-01-03 16:40:37 -0800

    Added card component.

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,26 +1,18 @@
 import React from 'react'
 import '../index.css'
 import { FaChromecast } from "react-icons/fa"
+import Card from './Card'
 
 const FeedbackItem = ({ feedback, reverse, deleteFeedback }) => {
-    // const deleteFeedback = (id) => {
-    //     console.log(`Deleting Feedback #${id}!`)
-    // }
-
+    
     return (
-        <div 
-            className="card" 
-            style={{
-                backgroundColor: reverse && 'blue',
-                color: reverse ? 'white' : 'yellow'
-            }}
-        >
+        <Card>
             <div className="rating">10</div>
             <button className="delete-card" onClick={() => deleteFeedback(feedback.id)}>
                 <FaChromecast />
             </button>
             <div className="text-display">{feedback.name}</div>
-        </div>
+        </Card>
     )
 }
 
